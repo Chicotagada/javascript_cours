0fa21b7697a891ac798707e506204baaff995b2b
Sebastien Chauvel <sebou.chauvel@gmail.com>
2018-09-05 17:48:41 +0200

    Add readme - add comments

--- JS/script.js
+++ JS/script.js
@@ -3,14 +3,14 @@
 //#################################################################
 //1_Créez votre premier code javascript
 
-/*var result = 'Sebastien Chauvel';
-alert(result);*/
+/*var result = 'Sebastien Chauvel';                     //déclare la variable
+alert(result);*/                                        //affiche la valeur de la variable
 
 //#################################################################
 //2_Utilisez les fonctions natives de Javascript
 
 /*var nom, prenom, age, result;
-nom = prompt('Veuillez saisir votre nom');
+nom = prompt('Veuillez saisir votre nom');              //demande à l'user de saisir un texte
 prenom = prompt('Veuillez saisir votre prénom');
 age = prompt('Veuillez saisir votre age');
 
@@ -60,20 +60,3 @@ else {
 
 //#################################################################
 //4_Utilisation des boucles_2
-
-do {
-  var age = prompt('Veuillez saisir votre âge :');
-  if (age < 18) {
-
-    alert('Vous n\'avez pas l\'age requis');
-
-  }
-  else if (age >= 18) {
-
-    alert('Bienvenue sur le site');
-    proceed = false;
-  }
-  else {
-    alert('Avez-vous saisi un chiffre ?');
-  }
-} while (proceed == true); // ou while (proceed);*/
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-javascript_cours
+Cours de Javascript - Université de Cergy-Pontoise - DUT Métiers du Multimédia et de l'internet - Sébastien Chauvel - 2018
