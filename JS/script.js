1bbb3caa140157fdb1260a9860cb65b67d4f01b3
Sebastien Chauvel <sebou.chauvel@gmail.com>
2018-09-05 17:39:32 +0200

    first TP

---
+++ JS/script.js
@@ -0,0 +1,79 @@
+//Cours de Sébastien Chauvel - DUT MMI - UCP
+
+//#################################################################
+//1_Créez votre premier code javascript
+
+/*var result = 'Sebastien Chauvel';
+alert(result);*/
+
+//#################################################################
+//2_Utilisez les fonctions natives de Javascript
+
+/*var nom, prenom, age, result;
+nom = prompt('Veuillez saisir votre nom');
+prenom = prompt('Veuillez saisir votre prénom');
+age = prompt('Veuillez saisir votre age');
+
+result = 'bonjour je m\'appelle ' + nom + ' ' + prenom + ' et j\'ai ' + age + ' ans';
+
+alert(result);*/
+
+//#################################################################
+//3_Tester vos variables et créez des conditions
+
+/*var age = prompt('Veuillez saisir votre âge :');
+
+if (age < 18) {
+
+alert('Vous n\'avez pas l\'age requis');
+
+}
+else if (age > 18) {
+
+alert('Bienvenue sur le site');
+}
+else {
+  alert('Avez-vous saisi un chiffre ?');
+}*/
+
+//#################################################################
+//4_Utilisation des boucles
+
+/*var proceed = true;
+
+do {
+var age = prompt('Veuillez saisir votre âge :');
+if (age < 18) {
+
+alert('Vous n\'avez pas l\'age requis');
+
+}
+else if (age >= 18) {
+
+alert('Bienvenue sur le site');
+proceed = false;
+}
+else {
+  alert('Avez-vous saisi un chiffre ?');
+}
+} while (proceed == true); // ou while (proceed);*/
+
+//#################################################################
+//4_Utilisation des boucles_2
+
+do {
+  var age = prompt('Veuillez saisir votre âge :');
+  if (age < 18) {
+
+    alert('Vous n\'avez pas l\'age requis');
+
+  }
+  else if (age >= 18) {
+
+    alert('Bienvenue sur le site');
+    proceed = false;
+  }
+  else {
+    alert('Avez-vous saisi un chiffre ?');
+  }
+} while (proceed == true); // ou while (proceed);*/
